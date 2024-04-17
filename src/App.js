d56da7543517a7fc7e347e6e32a841ce6370415b
Kendall Roy <kendallroy@blonded.local>
2024-04-17 13:47:39 -0400

    publish

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Collection } from '@aws-amplify/ui-react';
 import GameCard from './gamecard.js';
 import GamePage from './gamepage.js'; 
 import './App.css'
-import { DataStore } from '@aws-amplify/datastore';
 import React, { useState, useEffect } from 'react';
 import { Amplify } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/api';
 import config from './amplifyconfiguration.json';
-import { createTodo, updateTodo, deleteTodo } from './graphql/mutations';
 import { listTodos } from './graphql/queries';
-import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
@@ -19,43 +15,16 @@ Amplify.configure(config);
 
 
 const client = generateClient();
-/*
-const newTodo = await client.graphql({
-  query: createTodo,
-  variables: {
-      input: {
-  "title": "Lorem ipsum dolor sit amet",
-  "description": "Lorem ipsum dolor sit amet",
-  "price": 70,
-  "platform": "Lorem ipsum dolor sit amet",
-  "src": "Lorem ipsum dolor sit amet",
-  "releaseDate": "Lorem ipsum dolor sit amet",
-  "genre": "Lorem ipsum dolor sit amet",
-  "metacritic": 20,
-  "pos": 123.45,
-  "neg": 123.45,
-  "neu": 123.45
-}
-  }
-});
-*/
-const result = await client.graphql({ query: listTodos });
-console.log(result);
 
-
-//const result = await client.graphql({ query: listTodos });
-//console.log(result);
-
-
-
-
-//import { API, graphqlOperation } from 'aws-amplify';
 function App() {
   const [data, setData] = useState([]);
+  const [genreFilter, setGenreFilter] = useState(null);
+  const [priceFilter, setPriceFilter] = useState(null);
 
   useEffect(() => {
     fetchData();
   }, []);
+
   async function fetchData() {
     try {
       const result = await client.graphql({ query: listTodos });
@@ -64,64 +33,72 @@ function App() {
       console.error('Error fetching data:', error);
     }
   }
-  const games = data.map(item => ({
-    title: item.title, 
-    src: item.src, 
-    link: `/game/${item.title.toLowerCase().replace(/\s+/g, '-')}`, 
+
+  const games = data.filter(item => {
+    if (genreFilter && item.genre !== genreFilter) {
+      return false;
+    }
+    if (priceFilter && item.price > priceFilter) {
+      return false;
+    }
+    return true;
+  }).map(item => ({
+    title: item.title,
+    src: item.src,
+    link: `/game/${item.title.toLowerCase().replace(/\s+/g, '-')}`,
     neutral: item.neu,
     positive: item.pos,
     negative: item.neg,
     description: item.description,
     metacritic: item.metacritic,
-    genre:item.genre,
-    platform:item.platform,
-    releaseDate: item.releaseDate
+    genre: item.genre,
+    platform: item.platform,
+    releaseDate: item.releaseDate,
+    price: item.price 
   }));
 
-  /*[
-
-    {
-      title: 'Call of Duty',
-      src: 'https://upload.wikimedia.org/wikipedia/en/b/b1/Black_Ops_3.jpg',
-      link: '/call-of-duty',
-    },
-    {
-      title: 'Spider-Man 2',
-      src: 'https://image.api.playstation.com/vulcan/ap/rnd/202306/1219/2028edeaf4c0b60142550a3d6e024b6009853ceb9f51591e.jpg',
-      link: '/spider-man-2',
-    },
-    {
-      title: 'Minecraft',
-      src: 'https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_1240/b_white/f_auto/q_auto/ncom/software/switch/70010000000964/811461b8d1cacf1f2da791b478dccfe2a55457780364c3d5a95fbfcdd4c3086f',
-      link: '/minecraft',
-    },
-    // Other game objects
-  ];
-*/
-
-
-return (
-  <Router>
-    <div className="container">
-      <div className="center-text">S.A.B.E.R</div>
-      <div className="main-content">
-        <Routes>
-          <Route path="/" element={
-            <div>
-              {games.map((game, index) => (
-                <GameCard key={index} title={game.title} src={game.src} link={game.link} />
-              ))}
-            </div>
-          } />
-          {games.map((game) => (
-            <Route key={game.title} path={game.link} element={<GamePage game={game} />} />
-          ))}
-        </Routes>
+  return (
+    <Router>
+      <div className="container">
+        <div className="center-text">S.A.B.E.R</div>
+        <div className="main-content">
+          <div className="filters">
+            <label>
+              Genre:
+              <select value={genreFilter} onChange={(e) => setGenreFilter(e.target.value)}>
+                <option value="">All</option>
+                <option value="Action">Action</option>
+                <option value="Adventure">Adventure</option>
+                <option value="RPG">RPG</option>
+                <option value="Survival">Survival</option>
+                {/* Add more genres as needed */}
+              </select>
+            </label>
+            <label>
+              Max Price:
+              <input
+                type="number"
+                value={priceFilter}
+                onChange={(e) => setPriceFilter(Number(e.target.value))}
+              />
+            </label>
+          </div>
+          <Routes>
+            <Route path="/" element={
+              <div>
+                {games.map((game, index) => (
+                  <GameCard key={index} title={game.title} src={game.src} link={game.link} />
+                ))}
+              </div>
+            } />
+            {games.map((game) => (
+              <Route key={game.title} path={game.link} element={<GamePage game={game} />} />
+            ))}
+          </Routes>
+        </div>
       </div>
-    </div>
-  </Router>
-);
-
+    </Router>
+  );
 }
 
 export default App;
--- src/gamepage.js
+++ src/gamepage.js
@@ -1,43 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './gamepage.css'
+import './gamepage.css';
 import { Chart } from "react-google-charts";
 import Rating from './rating';
 
 function GamePage({ game }) {
-  
+
   const data = [
     ["Task", "Hours per Day"],
     ["Positive", game.positive],
     ["Negative", game.negative],
     ["Neutral", game.neutral]
-  ]
+  ];
   const options = {
     title: "Sentiment Analysis",
     backgroundColor: 'transparent',
     titleTextStyle: {
-      color: 'white', 
-      fontSize: 20 
+      color: 'white',
+      fontSize: 20
     },
     legendTextStyle: {
-      color: 'white', 
-      fontSize: 14 
+      color: 'white',
+      fontSize: 14
     },
-    height:500,
-    width:600
+    height: 400,
+    width: 600
   };
-  console.log(game)
- 
+
   const getColor = (rating) => {
     if (rating >= 80) {
-      return 'green'; 
+      return 'green';
     } else if (rating >= 60) {
-      return 'orange'; 
+      return 'orange';
     } else {
-      return 'red'; 
+      return 'red';
     }
-  }
-  
+  };
+
   return (
     <div className="game-page">
       <div className="game-header">
@@ -47,24 +46,24 @@ function GamePage({ game }) {
       <div className="game-details">
         <img src={game.src} alt={game.title} className="game-image" />
         <div className="description">
-          <p>Description:
-            {game.description}</p>
+          <p>Description: {game.description}</p>
+          <p>Price: ${game.price}</p>
           <p>Release Date: {game.releaseDate}</p>
           <p>Genre: {game.genre}</p>
           <p>Platform: {game.platform}</p>
+          <div className="rating-container">
+            <Rating score={game.metacritic} />
+          </div>
         </div>
-        <div className="chart-container">
-          <Chart
-            chartType="PieChart"
-            data={data}
-            options={options}
-            width={"100%"}
-            height={"400px"}
-          />
-        </div>
-        <div className="rating-container">
-        <Rating score={game.metacritic}/>
       </div>
+      <div className="chart-container">
+        <Chart
+          chartType="PieChart"
+          data={data}
+          options={options}
+          width={"100%"}
+          height={"400px"}
+        />
       </div>
     </div>
   );
